ac3aaa658ae9ca9c80743a9414e0930a95af4dd0
Josh Massa <joshmassa@gmail.com>
2024-03-18 16:59:07 -0400

    updated server.js, added data to employees.json

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 // Import required packages
 const inquirer = require('inquirer');
+
+// Query the user
+const inquiries = [
+    {
+        type: 'list',
+        name: 'employee tracker',
+        message: 'What would you like to do?',
+        choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add Department', 'Add Role', 'Add Employee', 'Update Employee Role', 'Quit']
+    }
+]
+
+// Initialize inquiries
+async function init() {
+    try {
+        const data = await inquirer.prompt(inquiries);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+init();
--- server.js
+++ server.js
@@ -2,13 +2,16 @@
 const express = require('express');
 const mysql = require('mysql2');
 
+// Import data from the employees.json file
+const { department, role, employee } = require('./db/employees.json');
+
 // Set PORT for server to use
 const PORT = process.env.PORT || 3001;
 
-// Initialize App
+// Initialize app
 const app = express();
 
-// Import Middleware
+// Import middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -20,9 +23,33 @@ const db = mysql.createConnection ({
     database: 'employees_db',
 }, console.log('Connected to employees_db'));
 
-// Set Homepage End Point
+// Set homepage end point
 app.get('/', (req, res) => {
     res.send('Welcome to our homepage!')
+});
+
+// Set end point to retrieve all departments 
+app.get('/api/departments', (req, res) => {
+    db.query('SELECT * FROM department', function (err, results) {
+        console.log(results);
+        res.status(200).json(results);
+    });
+});
+
+// Set end point to retrieve all roles
+app.get('/api/roles', (req, res) => {
+    db.query('SELECT * FROM role', function (err, results) {
+        console.log(results);
+        res.status(200).json(results);
+    });
+});
+
+// Set end point to retrieve all employees
+app.get('/api/employees', (req, res) => {
+    db.query('SELECT * FROM employee', function (err, results) {
+        console.log(results);
+        res.status(200).json(results);
+    });
 });
 
 // Listen For Requests To Server
