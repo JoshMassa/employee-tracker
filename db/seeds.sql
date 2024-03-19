8f2b109d28b6326ef7d16a49cd5bcd8532c51f46
Josh Massa <joshmassa@gmail.com>
2024-03-19 18:56:02 -0400

    updated employee table to include manager id, added functions to init function, created a fetchDepartments function for the add roll function, updated view functions

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,19 +11,10 @@ VALUES ('Software Engineer', 120000, 1),
        ('Quality Technician', 45000, 3),
        ('Quality Control Supervisor', 80000, 3);
 
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES  ('John', 'Pecan', 1),
-        ('Steve', 'Crumble', 2),
-        ('Jack', 'Rhubarb', 3),
-        ('Alyssa', 'Dandelion', 4),
-        ('Samantha', 'Clementine', 5),
-        ('Delilah', 'Wobblewhiskers', 6);
-
-UPDATE employee
-SET manager_id =
-    CASE
-        WHEN first_name = 'John' AND last_name = 'Pecan' THEN 2
-        WHEN first_name = 'Jack' AND last_name = 'Rhubarb' THEN 4
-        WHEN first_name = 'Samantha' AND last_name = 'Clementine' THEN 6
-    END
-WHERE first_name IN ('John', 'Jack', 'Samantha') AND last_name IN ('Pecan', 'Rhubarb', 'Clementine');
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('John', 'Pecan', 1, 2),
+        ('Steve', 'Crumble', 2, NULL),
+        ('Jack', 'Rhubarb', 3, 4),
+        ('Alyssa', 'Dandelion', 4, NULL),
+        ('Samantha', 'Clementine', 5, 6),
+        ('Delilah', 'Wobblewhiskers', 6, NULL);
--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const db = mysql.createConnection ({
     user: 'root',
     password: 'password',
     database: 'employees_db',
-}, console.log('Connected to employees_db'));
+}, function(err) {
+    if (err) throw err;
+    console.log('Connected to employees_db');
+});
 
 // Query the user
 const inquiries = [
@@ -30,6 +33,18 @@ async function init() {
         if (data.employee_tracker === "View All Departments") {
             viewDepts();
         }
+        if (data.employee_tracker === "View All Roles") {
+            viewRoles();
+        }
+        if (data.employee_tracker === "View All Employees") {
+            viewEmployees();
+        }
+        if (data.employee_tracker === "Add Department") {
+            addDept();
+        }
+        if (data.employee_tracker === "Add Role") {
+            addRole();
+        }
         if (data.employee_tracker === "Update Employee Role") {
             updateEmployee();
         }
@@ -40,10 +55,90 @@ async function init() {
 
 init();
 
+const fetchDepartments = async () => {
+    const getDepts = `SELECT id AS value, name FROM department;`;
+    const [departments] = await db.promise().query(getDepts);
+    return departments;
+};
+
 const viewDepts = () => {
     db.query('SELECT department.id, department.name AS departments FROM department', function (err, results) {
-        console.log(results);
+        console.table(results);
         init();
+    });
+};
+
+const viewRoles = () => {
+    db.query('SELECT role.id AS role_id, role.title, role.salary, role.department_id FROM role', function (err, results) {
+        console.table(results);
+        init();
+    });
+};
+
+const viewEmployees = () => {
+    db.query('SELECT employee.id, employee.first_name, employee.last_name, employee.manager_id, role.title AS role_title, role.salary, department.name AS department_name FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id;', function (err, results) {
+        console.table(results);
+        init();
+    });
+};
+
+const addDept = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'newDept',
+            message: 'Enter new department name:',
+        }
+    ]).then((answers) => {
+        db.query('INSERT INTO department (name) VALUES (?)', [answers.newDept], (err, results) => {
+            if (err) {
+                console.error('Error adding department:', error);
+            } else {
+            console.table(results);
+            init();
+            }
+        });
+    })
+    .catch((err) => {
+        console.error('Error:', error);
+    });
+};
+
+const addRole = async () => {
+    const choices = await fetchDepartments();
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'newRole',
+            message: 'Please enter a name for the new role:',
+        },
+        {
+            type: 'input',
+            name: 'newRoleSalary',
+            message: 'Please enter a salary for the new role:',
+        },
+        {
+            type: 'list',
+            name: 'newRoleDept',
+            message: 'Please assign the new role to one of the following departments:',
+            choices: choices.map(department => ({
+                name: department.name,
+                value: department.value
+            })),
+        }
+    ]).then((answers) => {
+        const title = answers.newRole;
+        const salary = answers.newRoleSalary
+        const departmentId = answers.department;
+
+        db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [title, salary, departmentId], (err, results) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log('New role added successfully');
+            init();
+        });
     });
 };
 
